3749d4781c9d97056522ebfcc89f19fa4b7376e1
Arutoru <arthurtankwa@gmail.com>
2025-12-25 04:49:33 +0100

    remove login requirement

--- move/views.py
+++ move/views.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class HomePageView(LoginRequiredMixin, TemplateView):
+class HomePageView(TemplateView):
     model = Point_vente
     template_name = "index.html"
